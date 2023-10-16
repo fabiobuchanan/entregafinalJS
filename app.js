b846abddd88c49e0a266ac329f48582b5cb4bce4
Fabio Buchanan <fabiobuchanan@gmail.com>
2023-10-16 20:40:48 -0300

    commit4

--- app.js
+++ app.js
@@ -62,44 +62,31 @@ class Producto {
 
 class ProductoController {
   constructor() {
-    this.listaProductos = [];
+    this.listaProductos = []
   }
 
   agregar(producto) {
-    this.listaProductos.push(producto);
+    if(producto instanceof Producto){
+    this.listaProductos.push(producto)
+    }
   }
 
-  cargarProductos(){
-    const p1 = new Producto(1, "Suscripción Mensual", "img/yoga1.jpg", "suscripción", 20, "Accedé a todas las clases durante un período de un mes.");
-    const p2 = new Producto(2, "Suscripción Semestral", "img/yoga2.jpg", "suscripción", 18, "Accedé a todas las clases durante un período de 6 meses.");
-    const p3 = new Producto(0, "Suscripción Anual", "img/yoga3.jpg", "suscripción", 16, "Accedé a todas las clases durante un período de un año.");
-    const p4 = new Producto(4, "Paquete 3 Clases", "img/yoga4.jpg", "clase", 10, "Accedé a las mejores clases de Yoga.");
-    const p5 = new Producto(5, "Paquete 5 Clases", "img/yoga5.jpg", "clase", 16, "Accedé a las mejores clases de Yoga.");
-    const p6 = new Producto(6, "Paquete 7 Clases", "img/yoga6.jpg", "clase", 24, "Accedé a las mejores clases de Yoga.");
-    const p7 = new Producto(7, "Paquete 9 Clases", "img/yoga7.jpg", "clase", 30, "Accedé a las mejores clases de Yoga.");
-    const p8 = new Producto(8, "Remera", "img/yoga8.jpg", "suscripción", 5000, "Los mejores productos de Yoga para una práctica cómoda y con buena tela.");
-    const p9 = new Producto(9, "Calza", "img/yoga1.jpg", "suscripción", 10000, "Los mejores productos de Yoga para una práctica cómoda y con buena tela.");
-    const p10 = new Producto(10, "Gorra", "img/yoga1.jpg", "suscripción", 4500, "Los mejores productos de Yoga para una práctica cómoda y con buena tela.");
-    const p11 = new Producto(11, "Sweater", "img/yoga1.jpg", "suscripción", 12000, "Los mejores productos de Yoga para una práctica cómoda y con buena tela.");
-    const p12 = new Producto(12, "Campera", "img/yoga1.jpg", "suscripción", 20000, "Los mejores productos de Yoga para una práctica cómoda y con buena tela.");
-      
-      CP.agregar(p1)
-      CP.agregar(p2)
-      CP.agregar(p3)
-      CP.agregar(p4)
-      CP.agregar(p5)
-      CP.agregar(p6)
-      CP.agregar(p7)
-      CP.agregar(p8)
-      CP.agregar(p9)
-      CP.agregar(p10)
-      CP.agregar(p11)
-      CP.agregar(p12)
+  async preparar_contenedor_productos(){
+    let listaProductosJSON = await fetch("./productos.json")
+    let listaProductosJS = await listaProductosJSON.json()
+
+    listaProductosJS.forEach(producto => {
+      let nuevoProducto = new Producto(producto.id, producto.nombre, producto.img, producto.categoria, producto.precio, producto.descripcion)
+      this.agregar(nuevoProducto)
+    })
+
+    this.mostrarEnDOM()
   }
 
   mostrarToastify(){
     Toastify({
-      text: "¡Producto Añadido!",
+      text: `¡${producto.nombre} añadido!`,
+      avatar: `${producto.img}`,
       duration: 2000,
       gravity: "bottom", // `top` or `bottom`
       position: "right", // `left`, `center` or `right`
@@ -170,7 +157,7 @@ class Carrito{
             //id, nombre, img, categoria, precio, descripcion
             let nuevoProducto = new Producto(producto.id, producto.nombre, producto.img, producto.categoria, producto.precio, producto.descripcion, producto.cantidad)
             listaAux.push(nuevoProducto)
-            this.listaCarrito = listaAux;
+            this.listaCarrito = listaAux
         })
       }
     }
@@ -243,8 +230,13 @@ class Carrito{
 
 const CP = new ProductoController()
 const carrito = new Carrito()
+
 carrito.recuperarStorage()
 carrito.mostrarEnDOM()
 carrito.eventoFinalizarCompra()
-CP.cargarProductos()
-CP.mostrarEnDOM()
+
+
+//CP.cargarProductos()
+//CP.mostrarEnDOM()
+
+CP.preparar_contenedor_productos()
